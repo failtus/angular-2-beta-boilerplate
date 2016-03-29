b7877592bd653e9a9ceda9b5f6892f24d1932965
Faitus <faitus@Faituss-iMac.local>
2016-03-30 02:17:44 +0600

    Clear last project

--- dev/app.component.ts
+++ dev/app.component.ts
@@ -1,12 +1,10 @@
 import {Component} from 'angular2/core';
-import {PuzzleComponent} from "./puzzle/puzzle.component";
 
 @Component({
     selector: 'my-app',
     template: `
-        <my-puzzle></my-puzzle>
+
     `,
-    directives: [PuzzleComponent]
 })
 export class AppComponent {
 
